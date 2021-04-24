85697e0a2cdb43a40e3716866138f265487f3578
Chijioke Ugwuanyi <chijioke@Chijiokes-MBP.lan>
2021-04-24 10:15:34 +0100

    cleaned up code.

--- app/routes/ApiRoutes.js
+++ app/routes/ApiRoutes.js
@@ -4,13 +4,7 @@ var ApiController = require('../controllers/ApiController.js');
 
 var middlewares = require("../utils/middleware.js");
 
-router.post('/signup', ApiController.createCustomer); //tick
-
 router.post('/auth/login', ApiController.logIn); //tick
-
-router.post('/auth/verify', ApiController.verifyCode); //tick
-
-router.post('/auth/verify/resend', ApiController.requestVerificationCode); //tick
 
 router.get('/auth/logout', middlewares.checkToken, ApiController.logOut);
 
@@ -21,12 +15,6 @@ router.post('/customer/add_to_cart', middlewares.checkToken, ApiController.addIt
 router.post('/customer/remove_from_cart', middlewares.checkToken, ApiController.removeItemFromCart); //tick
 
 router.post('/customer/make_purchase', middlewares.checkToken, ApiController.makeAPurchase); //tick
-
-router.get('/admin/customers', middlewares.checkToken,  ApiController.listCustomers); //tick
-
-router.get('/admin/orders/:user', middlewares.checkToken,  ApiController.listOrders);
-
-router.post('/admin/update_order', middlewares.checkToken, ApiController.updateOrder);
 
 router.use(function(req, res) {
     return res.status(404).send({ message: 'The url you visited does not exist.' });
