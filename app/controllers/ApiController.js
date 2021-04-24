01c8ddc470d968f9c9a73171da08e456152e1e0c
Chijioke Ugwuanyi <chijioke@Chijiokes-MBP.lan>
2021-04-24 19:50:42 +0100

    work done with the apis.

--- app/controllers/ApiController.js
+++ app/controllers/ApiController.js
@@ -1,75 +1,14 @@
-let ProductModel = require('../models/ProductModel.js');
 let ProductOrdersModel = require('../models/ProductOrdersModel.js');
 
 let config = require('../../config');
 
 module.exports = {
 
-    logIn: async function(req, res) {
-        if(!req.body.email || !req.body.password) return res.status(400).json({status: 400, message: "Email and password required."});
+    placeOrder: async function(req, res) {
+        if(!req.body.product_id) return res.status(400).json({status: 400, message: "Item required."});
 
         try {
-            let user = await UserModel.findOne({email: req.body.email.trim().toLowerCase()}).exec();
-            if(!user) return res.status(404).json({status: 404, message: 'Email or password incorrect.'});
-
-            let match = await bcrypt.compare(req.body.password.trim(), user.password.trim());
-
-            if (!match) return res.status(404).json({status: 404, success: false, message: 'Email or password incorrect.'});
-            
-            else {
-                if(!user.verified) {
-                    return res.status(400).json({status: 400, message: "Your email is yet to be verified. Check your mail for an activation code or request for a new link."})
-                } else {
-
-                    var token = jwt.sign({email: user.email, role: user.role}, secret, {expiresIn: 86400000});
-
-                    return res.status(200).json({status: 200, success: true,message: 'Have fun!',token: token});
-                }
-            }
-        } catch (error) {
-            return res.status(500).json({status: 500, message: 'Error processing requests.', error: error.message});
-        }
-    },
-
-    createItem: async function(req, res) {
-
-        if( !req.body.name || !req.body.item) return res.status(400).json({status: 400, message: "Your request cannot be processed. Supply missing fields."});
-
-        let admin = await UserModel.findOne({email: req.verified.email, role: "ADMIN"}).exec();
-        if(!admin) return res.status(404).json({status: 404, message: "Admin not found."});
-
-        cloudinary.uploader.upload(req.body.item, {public_id: "item" + imageId()},
-            function(error, result) {
-                if(error) return res.status(500).json({status: 500, message: 'Unable to process your request.', error: error });
-
-                var item = new ItemModel({
-                    item: result.secure_url,
-                    name: req.body.name,
-                    price: req.body.price,
-                    addedby: admin._id
-                });
-
-                item.save((err) => {
-                    if(err) return res.status(500).json({status: 500, message: 'Unable to process your request.', error: error});
-
-                    return res.status(200).json({status: 200, message: "Item added."});
-                });
-            }
-        );
-    },
-
-    addItemToCart: async function(req, res) {
-        if(!req.body.item) return res.status(400).json({status: 400, message: "Item required."});
-
-        try {
-            let user = await UserModel.findOne({email: req.verified.email, role: "CUSTOMER"}).exec();
-            if(!user) return res.status(404).json({status: 404, message: "User not found."});
-
-            let usercart = await ShoppingCartModel.findOne({user: user._id});
-            if(!usercart) return res.status(404).json({status: 404, message: "User's cart not found."});
-
-            if(usercart.items.includes(req.body.item))  return res.status(500).json({message: "Item already in shopping cart.", status: 500});
-
+            let o
             let updatecart = await ShoppingCartModel.findOneAndUpdate({user: user._id}, {$push: {"items" : req.body.item}}).exec();
             if(!updatecart) return res.status(500).json({message: "Unable to update shopping cart", status: 500});
             
@@ -138,3 +77,8 @@ module.exports = {
         }
     }
 }
+
+//come to a site. 
+//see a list of products
+//select one and place an order.
+//After placing an order. Communicate to products service to update order list.
